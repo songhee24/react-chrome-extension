4b971c0810d62ee52b9cd8cfe3bd6a01d5446977
songhee24 <akaratel61@gmail.com>
2023-08-29 16:07:48 +0600

    configured the react app rendering in extension

--- react-chrome-app/react-chrome-app/src/index.tsx
+++ react-chrome-app/react-chrome-app/src/index.tsx
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.createElement("div");
+rootElement.id = "react-chrome-app";
+
+const globalStyles = document.createElement("style");
+globalStyles.innerHTML = `
+  #${rootElement.id} {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 300px;
+  height: 100vh;
+  background: #ffffff;
+  border-right: 1px solid #c2c2c2;
+  z-index: 999;
+  }
+`;
+rootElement.appendChild(globalStyles);
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
