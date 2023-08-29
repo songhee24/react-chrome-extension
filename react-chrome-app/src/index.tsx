c607aa56fb46ce22c8adc3c39b6e212d0784ba84
songhee24 <akaratel61@gmail.com>
2023-08-29 16:31:38 +0600

    trying to build

--- react-chrome-app/src/index.tsx
+++ react-chrome-app/src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.createElement("div");
 rootElement.id = "react-chrome-app";
